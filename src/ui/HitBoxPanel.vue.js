366737947d131ebe6d7abc336b198c8a507d84df
Atlas Orchestrator <atlas@machine-max-plugin.dev>
2026-05-17 12:49:37 +0800

    refactor(ui): 提取 HitBoxPanel 为独立 Vue 组件

---
+++ src/ui/HitBoxPanel.vue.js
@@ -0,0 +1,68 @@
+/**
+ * 碰撞箱属性面板子组件
+ * 选中标记为 hit_box 的 Group 时在属性面板中显示
+ */
+Vue.component('mm-hit-box-panel', {
+    template: typeof TEMPLATE_HIT_BOX_PANEL !== 'undefined' ? TEMPLATE_HIT_BOX_PANEL : '<p>碰撞箱面板加载中...</p>',
+    props: {
+        config: { type: Object, required: true },
+        elementName: { type: String, default: '' },
+        parentSubPartKey: { type: String, default: '' },
+        materialDefs: { type: Object, default: function() { return {}; } },
+    },
+    data: function () {
+        return {
+            overwriteExpanded: false,
+        };
+    },
+    computed: {
+        badgeColor: function () { return '#D94A4A'; },
+        badgeLabel: function () { return '碰撞箱'; },
+        cubeCount: function () {
+            if (!this._parentGroup || !this._parentGroup.children) return 0;
+            return this._parentGroup.children.filter(function(c) { return c instanceof Cube; }).length;
+        },
+        cubeDimensions: function () {
+            var group = this._parentGroup;
+            if (!group || !group.children) return null;
+            var cubes = group.children.filter(function(c) { return c instanceof Cube; });
+            if (cubes.length === 0) return null;
+            var min = [Infinity, Infinity, Infinity];
+            var max = [-Infinity, -Infinity, -Infinity];
+            for (var i = 0; i < cubes.length; i++) {
+                var c = cubes[i];
+                for (var a = 0; a < 3; a++) {
+                    if (c.from[a] < min[a]) min[a] = c.from[a];
+                    if (c.to[a] > max[a]) max[a] = c.to[a];
+                }
+            }
+            var size = [max[0] - min[0], max[1] - min[1], max[2] - min[2]];
+            return size[0].toFixed(2) + ' × ' + size[1].toFixed(2) + ' × ' + size[2].toFixed(2);
+        },
+        cubePosition: function () {
+            var group = this._parentGroup;
+            if (!group) return null;
+            var pos = group.origin || [0, 0, 0];
+            return '(' + pos[0].toFixed(2) + ', ' + pos[1].toFixed(2) + ', ' + pos[2].toFixed(2) + ')';
+        },
+        _parentGroup: function () {
+            var el = this.$parent && this.$parent.selectedElement;
+            if (!el) return null;
+            if (el instanceof Group) return el;
+            return el.parent instanceof Group ? el.parent : null;
+        },
+    },
+    methods: {
+        onFieldChange: function (field, value) {
+            this.$emit('field-change', field, value);
+        },
+        onOverwriteChange: function (field, value) {
+            this.$emit('overwrite-change', field, value);
+        },
+        overwriteVal: function (field) {
+            return this.config.overwrite ? this.config.overwrite[field] : undefined;
+        },
+    },
+});
+
+module.exports = {};
